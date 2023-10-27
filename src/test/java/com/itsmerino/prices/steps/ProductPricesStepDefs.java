9c785c24567bc3e10072e35559d1e6db765f3431
Jorge Merino <jmerinoandres@gmail.com>
2023-10-27 21:38:59 +0200

    refactor: acceptance test

--- src/test/java/com/itsmerino/prices/steps/ProductPricesStepDefs.java
+++ src/test/java/com/itsmerino/prices/steps/ProductPricesStepDefs.java
@@ -1,57 +1,67 @@
 package com.itsmerino.prices.steps;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.itsmerino.prices.PricesApplication;
+import com.itsmerino.prices.application.ProductPriceResponse;
+import com.itsmerino.prices.shared.Constants;
 import io.cucumber.datatable.DataTable;
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
 import java.net.URI;
+import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
+import static com.itsmerino.prices.shared.Constants.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @CucumberContextConfiguration
+@SpringBootTest(classes = PricesApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class ProductPricesStepDefs {
 
-    private static final String PRODUCTS_URI = "http://localhost/api/v1/products?productId=%d&brandId=%d&date=%s";
+    private final DateTimeFormatter formatter;
+    private final ObjectMapper objectMapper;
 
-    private static final String PRODUCT_ID = "id";
-    private static final String BRAND_ID = "brandId";
-    private static final String RATE = "rate";
-    private static final String START_DATE = "startDate";
-    private static final String END_DATE = "endDate";
-    private static final String PRICE = "price";
-    private static final String CURRENCY = "currency";
+    private ProductPriceResponse productPriceResponse;
 
-    @Given("the following products:")
-    public void theFollowingProducts(DataTable dataTable) {
-        // TODO: save products
+    public ProductPricesStepDefs() {
+        this.formatter = DateTimeFormatter.ofPattern(Constants.DATE_FORMAT);
+        this.objectMapper = new ObjectMapper().registerModule(
+                new JavaTimeModule().addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(formatter))
+        );
     }
 
     @When("a customer requests the product {int} of the brand {int} at date {string}")
-    public void aCustomerRequestsTheProductOfTheBrandAtDate(Integer productId,
-                                                            Integer brandId,
-                                                            String dateTime) {
-        HttpRequest.newBuilder()
-                .uri(URI.create(String.format(PRODUCTS_URI, productId, brandId, dateTime)))
+    public void aCustomerRequestsTheProductOfTheBrandAtDate(Integer productId, Integer brandId, String dateTime) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(String.format(API + BASE_PATH + PRODUCT_PRICES_PATH + PRODUCT_PRICES_PARAMS, productId, brandId, dateTime)))
                 .GET()
                 .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        // TODO: get body and map to product
+        productPriceResponse = objectMapper.readValue(response.body(), ProductPriceResponse.class);
     }
 
-    @Then("the product is returned with the following data:$")
+    @Then("the product is returned with the following data:")
     public void theProductIsReturnedWithTheFollowingData(DataTable dataTable) {
         Map<String, String> productData = dataTable.asMap();
 
-        assertEquals(productData.get(PRODUCT_ID), "productId");
-        assertEquals(productData.get(BRAND_ID), "brandId");
-        assertEquals(productData.get(RATE), "rate");
-        assertEquals(productData.get(START_DATE), "startDate");
-        assertEquals(productData.get(END_DATE), "endDate");
-        assertEquals(productData.get(PRICE), "price");
-        assertEquals(productData.get(CURRENCY), "currency");
+        assertEquals(Integer.parseInt(productData.get("productId")), productPriceResponse.getProductId());
+        assertEquals(Integer.parseInt(productData.get("brandId")), productPriceResponse.getBrandId());
+        assertEquals(Integer.parseInt(productData.get("rate")), productPriceResponse.getRate());
+        assertEquals(productData.get("startDate"), productPriceResponse.getStartDate().format(formatter));
+        assertEquals(productData.get("endDate"), productPriceResponse.getEndDate().format(formatter));
+        assertEquals(Double.parseDouble(productData.get("price")), productPriceResponse.getPrice());
+        assertEquals(productData.get("currency"), productPriceResponse.getCurrency().toString());
     }
 }
--- src/test/resources/features/product-prices.feature
+++ src/test/resources/features/product-prices.feature
@@ -4,31 +4,23 @@ Feature: Get product prices
   I want to get the prices of the products for different dates
   So that I can buy them
 
-  Background:
-    Given the following products:
-    | ProductId | BrandId | Priority | Rate | StartDate           | EndDate             | Price | Currency |
-    | 35455     | 1       | 0        | 1    | 2020-06-14-00.00.00 | 2020-12-31-23.59.59 | 35.50 | EUR      |
-    | 35455     | 1       | 1        | 2    | 2020-06-14-15.00.00 | 2020-06-14-18.30.00 | 25.45 | EUR      |
-    | 35455     | 1       | 1        | 3    | 2020-06-15-00.00.00 | 2020-06-15-11.00.00 | 30.50 | EUR      |
-    | 35455     | 1       | 1        | 4    | 2020-06-15-16.00.00 | 2020-12-31-23.59.59 | 38.95 | EUR      |
-
   Scenario: Request at 10:00 on the 14th for product 35455 and brand 1
     When a customer requests the product 35455 of the brand 1 at date "2020-06-14-10.00.00"
     Then the product is returned with the following data:
-    | Field     | Value                |
-    | id        | 35455                |
-    | brandId   | 1                    |
-    | rate      | 1                    |
-    | startDate | 2020-06-14-00.00.00  |
-    | endDate   | 2020-12-31-23.59.59  |
-    | price     | 35.50                |
-    | currency  | EUR                  |
+      | Field     | Value                |
+      | productId | 35455                |
+      | brandId   | 1                    |
+      | rate      | 1                    |
+      | startDate | 2020-06-14-00.00.00  |
+      | endDate   | 2020-12-31-23.59.59  |
+      | price     | 35.50                |
+      | currency  | EUR                  |
 
   Scenario: Request at 16:00 on the 14th for product 35455 and brand 1
     When a customer requests the product 35455 of the brand 1 at date "2020-06-14-16.00.00"
     Then the product is returned with the following data:
       | Field     | Value                |
-      | id        | 35455                |
+      | productId | 35455                |
       | brandId   | 1                    |
       | rate      | 2                    |
       | startDate | 2020-06-14-15.00.00  |
@@ -40,7 +32,7 @@ Feature: Get product prices
     When a customer requests the product 35455 of the brand 1 at date "2020-06-14-21.00.00"
     Then the product is returned with the following data:
       | Field     | Value                |
-      | id        | 35455                |
+      | productId | 35455                |
       | brandId   | 1                    |
       | rate      | 1                    |
       | startDate | 2020-06-14-00.00.00  |
@@ -52,7 +44,7 @@ Feature: Get product prices
     When a customer requests the product 35455 of the brand 1 at date "2020-06-15-10.00.00"
     Then the product is returned with the following data:
       | Field     | Value                |
-      | id        | 35455                |
+      | productId | 35455                |
       | brandId   | 1                    |
       | rate      | 3                    |
       | startDate | 2020-06-15-00.00.00  |
@@ -64,7 +56,7 @@ Feature: Get product prices
     When a customer requests the product 35455 of the brand 1 at date "2020-06-16-21.00.00"
     Then the product is returned with the following data:
       | Field     | Value                |
-      | id        | 35455                |
+      | productId | 35455                |
       | brandId   | 1                    |
       | rate      | 4                    |
       | startDate | 2020-06-15-16.00.00  |
