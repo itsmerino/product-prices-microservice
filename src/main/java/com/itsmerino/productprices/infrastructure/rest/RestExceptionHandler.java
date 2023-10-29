ab88798806030e65014b96d1b8b76e129cdc7112
Jorge Merino <jmerinoandres@gmail.com>
2023-10-29 10:38:11 +0100

    refactor: add i18n

--- src/main/java/com/itsmerino/productprices/infrastructure/rest/RestExceptionHandler.java
+++ src/main/java/com/itsmerino/productprices/infrastructure/rest/RestExceptionHandler.java
@@ -1,15 +1,23 @@
 package com.itsmerino.productprices.infrastructure.rest;
 
 import com.itsmerino.productprices.domain.ProductPriceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-import static com.itsmerino.productprices.shared.Constants.BAD_REQUEST_MESSAGE;
-
 @ControllerAdvice
 public class RestExceptionHandler {
+
+    private final MessageSource messageSource;
+
+    @Autowired
+    public RestExceptionHandler(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<ErrorResponse> handleBadRequestException(MethodArgumentTypeMismatchException exception) {
@@ -26,7 +34,11 @@ public class RestExceptionHandler {
     }
 
     private ErrorResponse buildErrorResponse(MethodArgumentTypeMismatchException exception) {
-        String message = String.format(BAD_REQUEST_MESSAGE, exception.getName());
+        String message = messageSource.getMessage(
+                "bad-request.message",
+                new String[]{ exception.getName() },
+                LocaleContextHolder.getLocale()
+        );
 
         return ErrorResponse.builder()
                 .message(message)
--- src/main/java/com/itsmerino/productprices/shared/Constants.java
+++ src/main/java/com/itsmerino/productprices/shared/Constants.java
@@ -5,6 +5,4 @@ public class Constants {
     private Constants() {}
 
     public static final String DATE_FORMAT = "yyyy-MM-dd-HH.mm.ss";
-
-    public static final String BAD_REQUEST_MESSAGE = "Parameter [%s] is invalid";
 }
